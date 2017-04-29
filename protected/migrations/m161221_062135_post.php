ac4bcfa720da6863460083b3b150e46aeb73102d
shubham3694 <shubham@hitwicket.com>
2017-04-29 13:15:40 +0530

    indentation

--- protected/migrations/m161221_062135_post.php
+++ protected/migrations/m161221_062135_post.php
@@ -2,24 +2,24 @@
 
 class m161221_062135_post extends CDbMigration
 {
-public function safeUp() {
-$this->createTable(
-'post',
-array(
-'id'=>'int(11) UNSIGNED NOT NULL AUTO_INCREMENT',
-'user_id' => 'int(11) UNSIGNED NOT NULL',
-'content' => 'varchar(255) NOT NULL',
-'status' => 'TINYINT(1)',
-'created_at' => 'int(11)',
-'updated_at' => 'int(11)',
-'PRIMARY KEY (id)',
-),
-'ENGINE=InnoDB'
-);
-}
+	public function safeUp() {
+		$this->createTable(
+			'post',
+			array(
+				'id'=>'int(11) UNSIGNED NOT NULL AUTO_INCREMENT',
+				'user_id' => 'int(11) UNSIGNED NOT NULL',
+				'content' => 'varchar(255) NOT NULL',
+				'status' => 'TINYINT(1)',
+				'created_at' => 'int(11)',
+				'updated_at' => 'int(11)',
+				'PRIMARY KEY (id)',
+				),
+			'ENGINE=InnoDB'
+			);
+	}
 
-public function safeDown() {
-$this->dropTable("post");
+	public function safeDown() {
+		$this->dropTable("post");
 
-}
+	}
 }
